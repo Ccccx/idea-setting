4b3be6ac994d82e0cf6b1713915fc4eede6c2e59
chengjinzhou <cx5210@vip.qq.com>
2020-01-04 09:30:17 +0800

    IU-2019.3.1 <cx521@Ccx Create markdown.xml, IntelliLang.xml, print.xml, project.default.xml, diff.xml, Bean.java, myself.xml, yiiguxing.translation.xml, Default.xml, file.template.settings.xml, ignore.xml, find.xml, File Header.java, Visual Studio 2015 Dark.icls, Monokai.icls, KeyPromoterXSettings.xml, _@user_Darcula.icls, Darcula copy.icls, Relax Your Eyes.icls, mavenVersion.xml, GrepConsole.xml, Ergo1.icls, React.xml, Ergo.icls, Default.xml, Monokai_2.xml, Eclipse copy.xml, plain.xml, editor.xml, uml.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, other.xml, git.xml, _@user_Default.icls, ide-features-trainer.xml, rainbow_brackets.xml, lombok-plugin.xml, ui.lnf.xml, gradle.settings.xml, filetypes.xml, projectView.xml, javaeeExternalResources.xml, remote-servers.xml, plugin_ui.xml, colors.scheme.xml, junitgenerator-app-settings.xml, databaseDrivers.xml, github.xml, templates.xml, output.xml, hg.xml, laf.xml, parameter.hints.xml, Default1.icls, Class.java, jdk.table.xml, path.macros.xml, mavenRunHelper.xml, intentionSettings.xml, Default _1_.xml, web-browsers.xml, wsl.distributions.xml, proxy.settings.xml, vcs.xml, databaseSettings.xml, Eclipse copy 1.xml, toStringTemplates.xml, mybatis.xml, usageView.xml, 1.icls

---
+++ fileTemplates/Bean.java
@@ -0,0 +1,7 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+
+import lombok.Data;
+#parse("File Header.java")
+@Data
+public class ${NAME} {
+}
---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,6 @@
+/**
+ *
+ * @author chengjz
+ * @date  ${YEAR}-${MONTH}-${DAY} ${TIME}
+ * @version 1.0
+ */
